c388435b893a696d07072d96ce9fb7ab902e8533
Raypcte <rayanevalak@gmail.com>
2022-08-20 23:25:58 -0300

    questao 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from './renderWithRounter';
+
+describe('Teste o componente Not Found', () => {
+  it('Teste se a página contém um heading h2', () => {
+    renderWithRouter(<NotFound />);
+
+    const h2 = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(h2).toBeInTheDocument();
+
+    const imagem = screen.getByRole('img', { name: /pikachu crying/i });
+    expect(imagem).toBeInTheDocument();
+    expect(imagem).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
